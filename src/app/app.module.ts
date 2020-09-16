d8a033e1e9f60d8c6ec4c454a2d20c42c33ddeb9
NikitaJadhav123 <nrj.niki@gmail.com>
2020-09-16 18:39:15 +0530

    checkpoint 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home/home.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
